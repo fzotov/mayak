b4c7ac45cc4700bd8342031eb1f8e56a55ca357b
fzotov <fedor.zotov@vizbl.us>
2026-06-22 00:59:23 -0700

    restore TenantNew

--- src/pages/TenantNew.tsx
+++ src/pages/TenantNew.tsx
@@ -178,11 +178,10 @@ export function TenantNewPage({ onBack, onSaved }: { onBack: () => void; onSaved
         </div>
         <div style={grid3}>
           <div><label style={label}>ИНН</label><input style={inp} value={form.inn} onChange={f('inn')} placeholder={form.type === 'IP' ? '500700000000' : '7700000000'} /></div>
-          <div><label style={label}>{form.type === 'COMPANY' ? 'КПП' : 'КПП (не требуется)'}</label><input style={inp} value={form.kpp} onChange={f('kpp')} placeholder="770001001" disabled={form.type !== 'COMPANY'} /></div>
-          <div><label style={label}>{form.type === 'IP' ? 'ОГРНИП' : form.type === 'COMPANY' ? 'ОГРН' : 'Паспорт'}</label><input style={inp} value={form.ogrn} onChange={f('ogrn')} placeholder={form.type === 'IP' ? '315500700000000' : '1027700000000'} /></div>
+          <div><label style={label}>КПП {form.type !== 'COMPANY' ? '(не требуется)' : ''}</label><input style={inp} value={form.kpp} onChange={f('kpp')} placeholder="770001001" disabled={form.type !== 'COMPANY'} /></div>
+          <div><label style={label}>{form.type === 'IP' ? 'ОГРНИП' : form.type === 'COMPANY' ? 'ОГРН' : 'Паспорт серия/номер'}</label><input style={inp} value={form.ogrn} onChange={f('ogrn')} placeholder={form.type === 'IP' ? '315500700000000' : '1027700000000'} /></div>
           <div><label style={label}>Email</label><input style={inp} type="email" value={form.email} onChange={f('email')} placeholder="email@company.ru" /></div>
           <div><label style={label}>Телефон</label><input style={inp} value={form.phone} onChange={f('phone')} placeholder="+7 916 000-00-00" /></div>
-          <div><label style={label}>Факс / доп. контакт</label><input style={inp} placeholder="+7 916 000-00-00" /></div>
         </div>
         <div style={{ marginTop: 12 }}>
           <label style={label}>Юридический адрес</label>
@@ -199,7 +198,7 @@ export function TenantNewPage({ onBack, onSaved }: { onBack: () => void; onSaved
         <div style={grid3}>
           <div style={{ gridColumn: '1/3' }}><label style={label}>Расчётный счёт</label><input style={inp} value={form.bank_account} onChange={f('bank_account')} placeholder="40702810000000000000" /></div>
           <div><label style={label}>БИК</label><input style={inp} value={form.bik} onChange={f('bik')} placeholder="044525225" /></div>
-          <div style={{ gridColumn: '1/-1' }}><label style={label}>Корреспондентский счёт</label><input style={inp} value={form.corr_account} onChange={f('corr_account')} placeholder="30101810400000000225" /></div>
+          <div style={{ gridColumn: '1/3' }}><label style={label}>Корреспондентский счёт</label><input style={inp} value={form.corr_account} onChange={f('corr_account')} placeholder="30101810400000000225" /></div>
         </div>
       </div>
 
